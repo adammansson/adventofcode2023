2136b377196979d8ff2516ca53b7e8bf4f1b82a1
Adam Månsson <adam.s.mansson@gmail.com>
2023-12-19 14:40:58 +0100

    Refactor day 10 part 1

--- src/main/scala/aoc/day10/Part1.scala
+++ src/main/scala/aoc/day10/Part1.scala
@@ -4,9 +4,16 @@ import aoc.utils.*
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.ArrayBuffer
 
+enum Direction:
+	case North, East, South, West
+export Direction.*
+
 case class Point(x: Int, y: Int):
-	def add(dx: Int, dy: Int): Point =
-		Point(x + dx, y + dy)
+	def +(dir: Direction): Point = dir match
+		case North => Point(x, y - 1)
+		case East => Point(x + 1, y)
+		case South => Point(x, y + 1)
+		case West => Point(x - 1, y)
 
 def findStartingPoint(m: Vector[Vector[Char]]): Point =
 	val y = m.indexWhere(_.contains('S'))
@@ -15,26 +22,20 @@ def findStartingPoint(m: Vector[Vector[Char]]): Point =
 
 def nextPoints(m: Vector[Vector[Char]], p: Point): Set[Point] =
 	m(p.y)(p.x) match
-		case '|' => Set(p.add(0, -1), p.add(0, 1))
-		case '-' => Set(p.add(-1, 0), p.add(1, 0))
-		case 'L' => Set(p.add(0, -1), p.add(1, 0))
-		case 'J' => Set(p.add(0, -1), p.add(-1, 0))
-		case '7' => Set(p.add(0, 1), p.add(-1, 0))
-		case 'F' => Set(p.add(0, 1), p.add(1, 0))
+		case '|' => Set(p + North, p + South)
+		case '-' => Set(p + East, p + West)
+		case 'L' => Set(p + North, p + East)
+		case 'J' => Set(p + North, p + West)
+		case '7' => Set(p + South, p + West)
+		case 'F' => Set(p + South, p + East)
 		case _ => Set()
 
 @main def part1 =
 	val matrix = PuzzleInput(10).toVector.map(_.toCharArray().toVector)
-
 	val startingPoint = findStartingPoint(matrix)
-	var previousPoint = startingPoint
-	var currentPoint = startingPoint.add(1, 0)
 
-	val trail = ArrayBuffer[Point](startingPoint, currentPoint)
-	while currentPoint != startingPoint do
-		val nextPoint = (nextPoints(matrix, currentPoint) - previousPoint).head
-		trail += nextPoint
-		previousPoint = currentPoint
-		currentPoint = nextPoint
+	val trail = ArrayBuffer(startingPoint, startingPoint + East)
+	while trail.last != startingPoint do
+		trail += (nextPoints(matrix, trail.last) - trail(trail.length - 2)).head
 
 	println(trail.length / 2)
