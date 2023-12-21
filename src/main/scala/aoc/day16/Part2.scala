1a4974217b84a55e7f74aa50d0af56165897e738
Adam Månsson <adam.s.mansson@gmail.com>
2023-12-21 22:37:09 +0100

    Refactor day 16

--- src/main/scala/aoc/day16/Part2.scala
+++ src/main/scala/aoc/day16/Part2.scala
@@ -3,20 +3,18 @@ package aoc.day16
 import aoc.utils.*
 import scala.collection.mutable.ArrayBuffer
 
-def maxFromEdge(matrix: Vector[Vector[Char]], xs: Range, ys: Range, dir: Dir): Long =
-	(for {x <- xs; y <- ys} yield (x, y)).map((x, y) => tilesEnergized(matrix, Beam(Pos(x, y), dir))).max
-
 @main def part2 =
 	val matrix = PuzzleInput(16).toVector.map(_.toVector)
 
+	val fullRange = 0 until matrix.length
 	val firstRange = 0 to 0
 	val lastRange = (matrix.length - 1) to (matrix.length - 1)
 
 	val result = Vector(
-		maxFromEdge(matrix, matrix(0).indices, firstRange, South),
-		maxFromEdge(matrix, matrix(0).indices, lastRange, North),
-		maxFromEdge(matrix, firstRange, matrix.indices, East),
-		maxFromEdge(matrix, lastRange, matrix.indices, West),
-	).max
+		(fullRange, lastRange, North),
+		(firstRange, fullRange, East),
+		(fullRange, firstRange, South),
+		(lastRange, fullRange, West),
+	).map((xs, ys, dir) => (for {x <- xs; y <- ys} yield tilesEnergized(matrix, Beam(Pos(x, y), dir))).max).max
 
 	println(result)
